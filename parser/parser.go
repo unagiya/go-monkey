d2ccbb2685470cf0e04a36a90b8d9f0499aaef68
unagiya <big.stone.net@gmail.com>
2023-06-27 08:46:43 +0900

    add parser and ast

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"github.com/unagiya/go-monkey/ast"
+	"github.com/unagiya/go-monkey/lexer"
+	"github.com/unagiya/go-monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) parseProgram() *ast.Program {
+	return nil
+}
